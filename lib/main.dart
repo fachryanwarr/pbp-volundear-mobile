5a5b82ba123f82f7e97a3dc01d25b274af2f2123
mohamadarvin25 <mohamadarvin84@gmail.com>
2022-12-12 17:23:05 +0700

    hapus drawer

--- lib/main.dart
+++ lib/main.dart
@@ -35,92 +35,20 @@ class _MyHomePageState extends State<MyHomePage> {
   
 
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-  void _decreamentCounter() {
-    setState(() {
-      if (_counter > 0) {
-        _counter--;
-      }
-    });
-  }
+  
 
-  String showGanjilGenap() {
-    if (_counter % 2 == 0) {
-      return "Genap";
-    } else {
-      return "Ganjil";
-    }
-  }
-
-  Color changeGanjilGenapColor() {
-    if (_counter % 2 == 0) {
-      return Colors.red;
-    } else {
-      return Colors.blue;
-    }
-    
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
-    // listData.add(Model("andi", 110100, "Pemasukkan"));
-    // listData.add(Model("bagas", 235235, "Pengeluaran"));
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
 
-      drawer: myDrawer(context),
 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              showGanjilGenap(),
-              style: TextStyle(
-                color: changeGanjilGenapColor(),
-              ),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Stack(
-          children: <Widget>[
-            Visibility(
-              visible: _counter > 0,
-              child: Align(
-                  alignment: Alignment.bottomLeft,
-                  child: FloatingActionButton(
-                    onPressed: _decreamentCounter,
-                    tooltip: 'Decreament',
-                    child: const Icon(Icons.remove),
-                  ),
-                )
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
-              ),
-            ),
-          ],
-        ),
-      ), 
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      
 
     );
   }
