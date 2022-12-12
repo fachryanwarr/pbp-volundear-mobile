d7eb2f700bdc2993aa35b208234056d2f9506a74
Qistina Muharrifa <qistinamuharrifa@qistina.local>
2022-12-12 22:58:08 +0700

    update gamber crsl

--- lib/pages/donasi_page.dart
+++ lib/pages/donasi_page.dart
@@ -40,37 +40,18 @@ class _DonasiPage extends State<DonasiPage> {
                   color: Colors.white,
                 ),
               ),),
-            CarouselSlider(
-            options: CarouselOptions(
-              height: 400,
-              aspectRatio: 16/9,
-              viewportFraction: 0.8,
-              initialPage: 0,
-              enableInfiniteScroll: true,
-              reverse: false,
-              autoPlay: true,
-              autoPlayInterval: Duration(seconds: 3),
-              autoPlayAnimationDuration: Duration(milliseconds: 800),
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enlargeCenterPage: true,
-              enlargeFactor: 0.3,
-              scrollDirection: Axis.horizontal,
-            ),
-            items: [1,2,3,4,5].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: BoxDecoration(
-                      color: Colors.amber
-                    ),
-                    child: Text('text $i', style: TextStyle(fontSize: 16.0),)
-                  );
-                },
-              );
-            }).toList(),
-          ),
+              CarouselSlider(
+      options: CarouselOptions(
+        autoPlay: true,
+        aspectRatio: 2.0,
+        enlargeCenterPage: true,
+      ),
+      items: [
+        Image.asset('assets/images/gambar3.webp'),
+        Image.asset('assets/images/gambar2.jpeg'),
+        Image.asset('assets/images/36951-ui.jpeg'),
+      ],
+    ),
           const Padding(
               padding: EdgeInsets.only(left: 15, top: 10),
               child: Text(
